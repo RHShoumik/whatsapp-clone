71385d186c2a810bbe68631e8f19663660d338cb
RHShoumik <rhshoumik12@gmail.com>
2023-03-14 02:37:16 +0600

    Last on sidebar design

--- src/layout/sidebar/Sidebar.js
+++ src/layout/sidebar/Sidebar.js
@@ -1,9 +1,33 @@
 import React from "react";
 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import GroupsIcon from '@mui/icons-material/Groups';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import "./Sidebar.scss"
+
 const Sidebar = () => {
   return (
     <div className="Sidebar">
-      <h1>hi</h1>
+      <div className="Sidebar--header">
+        <div className="Sidebar--header--left">
+          <AccountCircleIcon />
+        </div>
+        <div className="Sidebar--header--right">
+          <GroupsIcon />
+          <DonutLargeIcon />
+          <ChatIcon />
+          <MoreVertIcon />
+        </div>
+      </div>
+      <div className="Sidebar--search">
+
+      </div> 
+      <div className="Sidebar--chats">
+
+      </div>
     </div>
   )
 }
