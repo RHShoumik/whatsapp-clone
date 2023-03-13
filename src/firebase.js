a955635ef48b87a86ecccb121215faefbb48f576
RHShoumik <rhshoumik12@gmail.com>
2023-03-13 23:11:38 +0600

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYpGDYv3D2X1vKbi6wOUp1QdhwhTirhOs",
+    authDomain: "whatsapp-clone-b168f.firebaseapp.com",
+    projectId: "whatsapp-clone-b168f",
+    storageBucket: "whatsapp-clone-b168f.appspot.com",
+    messagingSenderId: "1081190681118",
+    appId: "1:1081190681118:web:aec6198af5562e58a77c0f",
+    measurementId: "G-413GYV82QW"
+  };
